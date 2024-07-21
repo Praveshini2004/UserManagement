6c6684a9b592851bc6a9d348db64607154d8f915
Praveshini2004 <146466699+Praveshini2004@users.noreply.github.com>
2024-07-21 22:18:46 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,25 @@
-import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './components/AddUser';
+import NavBar from './components/NavBar';
+import AllUser from './components/AllUser';
+import Welcome from './components/Welcome';
+import EditUser from './components/EditUser';
 
-const baseURL = 'https://user-management-frontend-gamma.vercel.app/'; // Replace with your actual API URL
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Function to get all users
-export const getAllUsers = async () => {
-  try {
-    const response = await axios.get(`${baseURL}/users`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    throw error;
-  }
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/all' element={<AllUser />} />
+        <Route path='/add' element={<AddUser />} />
+        <Route path='/edit/:id' element={<EditUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-// Function to get a user by ID
-export const getUserById = async (id) => {
-  try {
-    const response = await axios.get(`${baseURL}/users/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error(`Error fetching user with ID ${id}:`, error);
-    throw error;
-  }
-};
-
-// Function to add a new user
-export const addUser = async (user) => {
-  try {
-    const response = await axios.post(`${baseURL}/users`, user);
-    return response.data;
-  } catch (error) {
-    console.error('Error adding user:', error);
-    throw error;
-  }
-};
-
-// Function to update an existing user
-export const updateUser = async (id, user) => {
-  try {
-    const response = await axios.put(`${baseURL}/users/${id}`, user);
-    return response.data;
-  } catch (error) {
-    console.error(`Error updating user with ID ${id}:`, error);
-    throw error;
-  }
-};
-
-// Function to delete a user
-export const deleteUser = async (id) => {
-  try {
-    const response = await axios.delete(`${baseURL}/users/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error(`Error deleting user with ID ${id}:`, error);
-    throw error;
-  }
-};
+export default App;
